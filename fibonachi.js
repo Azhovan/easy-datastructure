23bcd1f298a4efc8554912db8214f809c97c0973
Azhovan Asadi <azhovan@one.fit>
2019-08-11 20:29:50 +0200

    fibonachi o(n)

---
+++ fibonachi.js
@@ -0,0 +1,14 @@
+function fibo(n, lastlast, last) {
+    if (n === 0)
+        return lastlast;
+
+    if (n === 1)
+        return last;
+
+    return fibo(n - 1, last, lastlast + last);
+}
+
+
+console.log(
+    fibo(200, 0, 1)
+);
